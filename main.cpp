1b29663c8c6d29095bc70416d24886951e273090
Aedan Pettit <apettit20@wooster.edu>
2019-10-22 01:21:10 -0400

    Added final documentation

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 /* Project 3 - Erika Goetz and Aedan Pettit
- * All code written by Erika and Aedan
- * This project displays a stick-figure runner that runs around the track
- * The animation can be toggled using the menu that is activated by a right
- * click.
+ * A man running on a track in OpenGL
+ *
+ * To run this program on Linux, make sure that both glut and openGL are installed.
+ * Use the command g++ main.cpp -lglut -lGL -lGLU -o runningto compile.
+ * Then run ./running.
+ * To run this program on Mac or Windows, make sure to compile with the libraries
+ * on your computer.
+ * Once in the program, a user may press 'esc' to quit.
+ * They may also right click on the window in order to speed up the runner,
+ * to slow down the runner, or to quit.
  */
 
 #ifdef __APPLE__
